845b8fc03396a7823093e817f46a8bacff3f64f5
DumbAugust <carlaugust.sponberg@gmail.com>
2024-05-16 13:33:29 +0200

    I do not know what is going on.

--- Sin animations/main.js
+++ Sin animations/main.js
@@ -13,7 +13,20 @@ function sinWave(k, x, m, a){
     return Math.sin(k * x + m) * a
 }
 
+function cosWave(k, x, m, a) {
+    return Math.cos(k * x+ m)*a
+}
+
+function tanWave(k,x,m,a) {
+    return Math.tan(k * x + m) * a
+}
+
+function cotWave(k,x,m,a) {
+    return 1/(Math.tan(k*x+m)*a)
+}
+
 let x = 0;
+let y = 0;
 let speed = 10;
 let m = 0
 
@@ -22,17 +35,17 @@ function animate(){
 
     requestAnimationFrame(animate);
     c.beginPath();
-    c.lineWidth = 5;
 
     for (let i = 0; i < cX; i++) {
-        c.lineTo(x, sinWave(0.02, x, m, 100) + cY, 1, 1);      
-        x++;
+        c.lineTo(tanWave(20, x, m, 2) + cX, cosWave(10, y, m, 1000) + cY , 1, 1, false);      
+        x += 1;
+        y += 1;
     }
 
     c.clearRect(0, 0, cX * 2, cY * 2);
     c.stroke();
-    m += 0.1;
     x = 0;
+    m += 0.3
     console.log("f");
 }
 
