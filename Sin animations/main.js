b0f115166c9433fb9215deb7ecda498ed1249674
DumbAugust <carlaugust.sponberg@gmail.com>
2024-05-20 22:19:11 +0200

    Just some varibles that crash if equal zero

--- Sin animations/main.js
+++ Sin animations/main.js
@@ -26,9 +26,10 @@ function cotWave(k,x,m,a) {
 }
 
 let x = 0;
-let y = 0;
 let speed = 10;
-let m = 0
+let m = 1
+let markerSpeed = 1.1
+let loopSpeed = 2/markerSpeed
 
 
 function animate(){
@@ -36,24 +37,24 @@ function animate(){
     requestAnimationFrame(animate);
     c.beginPath();
 
-    for (let i = 0; i < cX; i++) {
-        c.lineTo(tanWave(20, x, m, 2) + cX, cosWave(10, y, m, 1000) + cY , 1, 1, false);      
-        x += 1;
-        y += 1;
+    for (let i = 0; i < cX*loopSpeed; i++) {
+        c.beginPath
+        c.lineTo(x, cosWave(0.01, x, m, 100)*sinWave(0.1,x,m,2) + cY , 1, 1, false);   
+        x += markerSpeed;
     }
 
     c.clearRect(0, 0, cX * 2, cY * 2);
     c.stroke();
     x = 0;
-    m += 0.3
+    m += 0.01
     console.log("f");
 }
 
 function doStuff() {
     console.log("hello!");
+    console.log(loopSpeed)
     setTimeout(doStuff, 1000);
  }
 
 setTimeout(doStuff, 1000);
 animate();
-
